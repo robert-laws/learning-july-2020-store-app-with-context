4339fdbded0aaaa82951d92554e342447eafc626
Robert Laws <roblaws@me.com>
2020-07-28 11:39:32 +0300

    setup basic components and navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Products from './pages/Products';
+import Cart from './pages/Cart';
+
 import './App.scss';
 
 function App() {
   return (
     <div className='App'>
-      <h1>App</h1>
+      <Header />
+      <main>
+        <Router>
+          <Switch>
+            <Route exact path='/' component={Products} />
+            <Route exact path='/cart' component={Cart} />
+          </Switch>
+        </Router>
+      </main>
+      <Footer />
     </div>
   );
 }
